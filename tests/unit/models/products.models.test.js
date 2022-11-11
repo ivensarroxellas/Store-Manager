d0a10cd39eb159f35b9832d82af17447341edd72
Ivens <ivensarroxellas@hotmail.com>
2022-11-11 17:59:39 -0300

    req 2

--- tests/unit/models/products.models.test.js
+++ tests/unit/models/products.models.test.js
@@ -1,6 +1,6 @@
-/* const { expect } = require('chai');
+const { expect } = require('chai');
 const sinon = require('sinon');
-const { passengerModel } = require('../../../src/models');
+const { productModel } = require('../../../src/models');
 
 const connection = require('../../../src/models/connection');
 const { products } = require('../../mocks/products');
@@ -10,13 +10,13 @@ describe('Testes de unidade do model de pessoas passageiras', function () {
 
   it('Recuperando a lista de produtos', async function () {
     sinon.stub(connection, 'execute').resolves([products]);
-    const result = await passengerModel.findAll();
+    const result = await productModel.findAll();
     expect(result).to.be.deep.equal(products);
   });
 
   it('Recuperando um produto a partir do seu id', async function () {
     sinon.stub(connection, 'execute').resolves([[products[0]]]);
-    const result = await passengerModel.findById(1);
+    const result = await productModel.findById(1);
     expect(result).to.be.deep.equal(products[0]);
   });
-}); */
+});

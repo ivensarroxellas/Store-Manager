967ca03796557ff08c73911fdb8e37da37d56e7d
Ivens <ivensarroxellas@hotmail.com>
2022-11-14 13:08:51 -0300

    Connection fix

--- src/models/connection.js
+++ src/models/connection.js
@@ -9,3 +9,15 @@ const connection = mysql.createPool({
 });
 
 module.exports = connection;
+
+/* const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: 'db',
+  user: 'root',
+  password: 'password',
+  database: 'StoreManager',
+  port: 3306,
+});
+
+module.exports = connection; */

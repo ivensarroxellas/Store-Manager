724787cb06341c5b88fa2cf6331774ba0eccdea1
Ivens <ivensarroxellas@hotmail.com>
2022-11-14 13:05:41 -0300

    Connection fix

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'db',
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
-  port: 3306,
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: process.env.MYSQL_PORT || 3306,
+  user: process.env.MYSQL_USER || 'root',
+  password: process.env.MYSQL_PASSWORD || '1605',
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
 });
 
 module.exports = connection;
